ac44be3f2dbd32c99d80cc7ba72a529da75efca0
Bryan Yan <74737261+bryanyanay@users.noreply.github.com>
2022-07-10 13:20:32 -0400

    Add varint functions

--- myImplementation.py
+++ myImplementation.py
@@ -328,6 +328,29 @@ def int_to_little_endian(i, length):
   # if it is greater, then we get \x00 padded to the right 
   return i.to_bytes(length, "little")
 
+def read_varint(s):
+  # s is the byte stream (a BytesIO object i believe), and we're reading a single varint at the front of the stream
+  i = s.read(1)[0] 
+  if i == 0xfd:
+    return little_endian_to_int(s.read(2))
+  elif i == 0xfe:
+    return little_endian_to_int(s.read(4))
+  elif i == 0xff:
+    return little_endian_to_int(s.read(8))
+  else:
+    return i
+def encode_varint(i):
+  if i < 0xfd:
+    return bytes([i])
+  elif i < 0x10000:
+    return b'\xfd' + int_to_little_endian(i, 2)
+  elif i < 0x100000000:
+    return b'\xfe' + int_to_little_endian(i, 4)
+  elif i < 0x10000000000000000:
+    return b'\xff' + int_to_little_endian(i, 8)
+  else:
+    raise ValueError(f'integer too large: {i}')
+
 
 class Tx:
   def __init__(self, version, tx_ins, tx_outs, locktime, testnet = False):
